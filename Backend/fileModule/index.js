10504f3dd574f0168abd1a84b2082a4109714c7a
Avinash <avinashkumar200217@gmail.com>
2024-07-03 17:59:32 +0530

    fs module

--- Backend/fileModule/index.js
+++ Backend/fileModule/index.js
@@ -1,9 +1,22 @@
 const fs=require("fs")
 
-fs.writeFile("avinash.txt","Avinash is in love with Anshu and will forever will be.",(req,res)=>{
+fs.writeFile("avinash.txt","Avinash is in love with Anshu and will forever will be. ",(req,res)=>{
     console.log("file has been updated")
 })
 
 fs.readFile("avinash.txt", "utf-8",(req,data)=>{
     console.log(data)
 })
+
+fs.appendFile("avinash.txt", "He never thought that he will find someone so beautiful and kind.",(req,data)=>{
+    console.log("data has been appended");
+})
+
+fs.rmdir("test folder",(res,req)=>{
+    console.log("Folder has been deleted")
+})
+fs.unlink("test.txt",(res,data)=>{
+    console.log("file has been deleted")
+})
+
+fs.renameSync("avinash.txt","Anshu.txt") 
