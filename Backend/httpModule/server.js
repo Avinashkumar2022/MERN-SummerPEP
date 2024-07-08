4f0d8df5e20dfa49a397a6870f0341eb88e162d6
Avinash <avinashkumar200217@gmail.com>
2024-07-08 13:16:05 +0530

    minor changes

--- Backend/httpModule/server.js
+++ Backend/httpModule/server.js
@@ -1,18 +1,60 @@
 const http=require("http")
+const fs= require("fs")
+
+
+// const server=http.createServer((req,res)=>{
+//     const data=fs.readFileSync("./userapi.json","utf-8")
+
+//     const file=fs.readFileSync("./index.html","utf-8")
+
+//     const myData=JSON.parse(data)
+
+//     if(req.url==="/"){
+//         res.end(file)
+//     }
+//     else if(req.url==="/about")
+//     {
+//         res.end("<h1> About page </h1>")
+//     }
+    
+//     else if(req.url==="/contact")
+//     {
+//         res.end("<h1> Contact page </h1>")
+//     }
+//     else if(req.url==="/userapi")
+//     {
+//         res.end(myData[3].email)
+//     }
+//     else{
+//         res.end("Error Page")
+//     }
+    
+// }).listen(3000,()=>{console.log("listening to port 3000")})
+
+const path=require("path")
+
 const server=http.createServer((req,res)=>{
-    if(req.url==="/"){
-        res.end("<h1> Avinash Kumar </h1>")
+    let filepath=path.join(__dirname,"public",req.url==="/"?"index.html":req.url)
+
+    const mimiTypes={
+        "html":"text"
     }
-    else if(req.url==="/about")
-    {
-        res.end("<h1> About page </h1>")
-    }
-    
-    else if(req.url==="/contact")
-    {
-        res.end("<h1> Contact page </h1>")
-    }
-    
-}).listen(3000,()=>{
-    console.log("listening to port 3000")
+
+    fs.readFile(filepath,(err,content)=>{
+        if(err){
+            if(err.code==='ENOENT')
+            {
+                fs.readFile(path.join(__dirname,"public","404.html"),(error,content)=>{
+                    res.writeHead(404,{"Content-Type":"text/html"})
+                    res.end(content,"utf-8")
+                })
+            }
+            else{
+                res.writeHead(500,{"Content-Type"})
+            }
+        }
+        else{
+
+        }
+    })
 })
--- Backend/osModule/index.js
+++ Backend/osModule/index.js
@@ -7,3 +7,8 @@ const memory=(os.freemem()/(1024*1024*1024))
 console.log(memory)
 
 console.log("Total Memory: "+os.totalmem()/1024*1024*1024)
+
+console.log("Host name: "+os.hostname());
+console.log("Operating System name: "+os.type());
+console.log("OS platform: "+os.platform())
+console.log("OS release: "+ os.release())
